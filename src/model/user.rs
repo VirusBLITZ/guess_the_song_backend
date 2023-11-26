6699c7430c02a0cc23cc1a9ee437f4cd4a7d3694
VirusBLITZ <58221423+VirusBLITZ@users.noreply.github.com>
2023-11-27 00:41:14 +0100

    implement PartialEq for User

--- src/model/user.rs
+++ src/model/user.rs
@@ -8,7 +8,12 @@ type Message = String;
 pub struct User {
     pub id: u32,
     pub name: String,
-    pub score: u8,
     pub game_id: Option<u16>,
     pub ws: Option<Addr<UserSocket>>,
+}
+
+impl PartialEq for User {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
 }
